dc4ab38cbc0f320dff64225dc94ece5dc793372a
Unna27 <unnaweb27@gmail.com>
2021-10-12 10:24:49 +0200

    added IIFE to pokemonRepository

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-// Declare handle to access unordered list
+// Declare handle to access the unordered list to display pokemon details
 let datalist_row = document.getElementsByClassName('datatlist');
-// An array to store the pokemon objects. Each object has certain set of pokemon properties
-let pokemonList = [];
+
+// assign pokemonRepository an IIFE
+let pokemonRepository = (function(){
+  // An array to store pokemon objects.
+  let pokemonList = [];
+  // adds pokemon object to the pokemonList array
+  function add(pokemon){
+    pokemonList.push(pokemon);
+  }
+  // returns pokemonList array
+  function getAll(){
+    return pokemonList;
+  }
+  // returns handle to add and getAll functions
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 let pokemon1 = {name: "Squirtle", height: 0.5, weight: 9, types: ['water']};
 let pokemon2 = {name: "Beedrill", height: 1, weight: 29.5, types: ['bug','poison']};
 let pokemon3 = {name: "Nidoran", height: 0.5, weight: 9, types: ['poison']};
 let pokemon4 = {name: "Venomoth", height: 1.5, weight: 12.5, types: ['bug','poison']};
 let pokemon5 = {name: "Meganium", height: 1.8, weight: 100.5, types: ['grass']};
-pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5];
-
-console.log(pokemonList);
+pokemonRepository.add(pokemon1);
+pokemonRepository.add(pokemon2);
+pokemonRepository.add(pokemon3);
+pokemonRepository.add(pokemon4);
+pokemonRepository.add(pokemon5);
+console.log(pokemonRepository.getAll());
 // function to display all the Pokemon names that accepts two args (pokemon obj and its index in the array)
 function displayPokemonNames(pokemon, i) {
   let li=document.createElement('li');
@@ -23,4 +43,5 @@ function displayPokemonNames(pokemon, i) {
   datalist_row[0].appendChild(li);
 }
 // for each element in the pokemonlist invoke displayPokemonNames function to display its details
-pokemonList.forEach(displayPokemonNames);
+
+(pokemonRepository.getAll()).forEach(displayPokemonNames);
