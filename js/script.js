7695e59d51c3860bc0424014a044ea675da48392
Unna27 <unnaweb27@gmail.com>
2021-10-12 20:09:34 +0200

    added a check for datattype and property while pushing value to Repository.
    Added search feature by name.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-// Declare handle to access the unordered list to display pokemon details
+// Declare handles to access the unordered list and seach field name
 let datalist_row = document.getElementsByClassName('datatlist');
+let searchName = document.getElementById('search_name');
 
 // assign pokemonRepository an IIFE
 let pokemonRepository = (function(){
   // An array to store pokemon objects.
   let pokemonList = [];
+  let validkeys = {name: "Pokemonname", height: 0.0, weight: 0, types: []};
   // adds pokemon object to the pokemonList array
   function add(pokemon){
-    pokemonList.push(pokemon);
+    // check if its an object and has all the required properties/keys
+    if(typeof(pokemon) === 'object'){
+      if(Object.keys(validkeys).every((property) => property in pokemon )) {
+        pokemonList.push(pokemon);
+        return 'success';
+      }else {
+        return 'property_failure';
+      }
+    }else {
+      return  'type_failure';
+    }
   }
   // returns pokemonList array
   function getAll(){
@@ -19,17 +31,7 @@ let pokemonRepository = (function(){
     getAll: getAll
   };
 })();
-let pokemon1 = {name: "Squirtle", height: 0.5, weight: 9, types: ['water']};
-let pokemon2 = {name: "Beedrill", height: 1, weight: 29.5, types: ['bug','poison']};
-let pokemon3 = {name: "Nidoran", height: 0.5, weight: 9, types: ['poison']};
-let pokemon4 = {name: "Venomoth", height: 1.5, weight: 12.5, types: ['bug','poison']};
-let pokemon5 = {name: "Meganium", height: 1.8, weight: 100.5, types: ['grass']};
-pokemonRepository.add(pokemon1);
-pokemonRepository.add(pokemon2);
-pokemonRepository.add(pokemon3);
-pokemonRepository.add(pokemon4);
-pokemonRepository.add(pokemon5);
-console.log(pokemonRepository.getAll());
+
 // function to display all the Pokemon names that accepts two args (pokemon obj and its index in the array)
 function displayPokemonNames(pokemon, i) {
   let li=document.createElement('li');
@@ -38,10 +40,65 @@ function displayPokemonNames(pokemon, i) {
     li.innerHTML = (i + 1) + '. '+ pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!';
     li.setAttribute("class","spl_pokemon");
   } else {
-    li.innerHTML = (i +1 ) + '. '+ pokemon.name + ' (height: ' + pokemon.height + ')';
+    li.innerHTML = (i + 1) + '. '+ pokemon.name + ' (height: ' + pokemon.height + ')';
   }
   datalist_row[0].appendChild(li);
 }
-// for each element in the pokemonlist invoke displayPokemonNames function to display its details
 
+// function to check if a specific pokemon is in the pokemon repository by providing only its name
+function checkPokemonByName(pokemonList,name){
+  return pokemonList.filter(function(pokemon) {
+    if(pokemon.name.toLowerCase() === name.toLowerCase()) {
+          return pokemon;
+      }
+  });
+}
+// invoke checkPokemonByName with search query
+function searchPokemon() {
+  if(searchName.value === ""){
+    alert('Please enter a Pokemon name to be searched.');
+  }else {
+    let searchResult = checkPokemonByName(pokemonRepository.getAll(),searchName.value);
+    console.log(searchResult);
+    if(searchResult.length === 0){
+      alert('Pokemon ' + searchName.value + ' not found');
+    }else {
+      alert('Pokemon ' + searchName.value + ' found');
+    }
+  }
+}
+// input pokemonList
+let pokemon1 = {name: "Squirtle", height: 0.5, weight: 9, types: ['water']};
+let pokemon2 = {name: "Beedrill", height: 1, weight: 29.5, types: ['bug','poison']};
+let pokemon3 = {name: "Nidoran", height: 0.5, weight: 9, types: ['poison']};
+let pokemon4 = {name: "Venomoth", height: 1.5, weight: 12.5, types: ['bug','poison']};
+let pokemon5 = {name: "Meganium", height: 1.8, weight: 100.5, types: ['grass']};
+let inputPokemonList = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, "test"];
+let success_index = [];
+let property_failure_index = [];
+let type_failure_index = [];
+// add pokemonList to the repository array
+for (let i = 0; i < 6; i++){
+  let captureMessage = pokemonRepository.add(inputPokemonList[i]);
+  if (captureMessage === "success") {
+    success_index.push(i);
+  } else if (captureMessage === "property_failure") {
+    property_failure_index.push(i);
+  }
+  else {
+    type_failure_index.push(i);
+  }
+}
+// display alert message to tell the status of pokemon addition to the repository from the input list
+if(success_index.length !== 0){
+  alert("Pokemons at the following indexes are added successfully.\n" + success_index);
+}
+if(property_failure_index.length !== 0){
+  alert("Pokemons at the following indexes are not added due to some of the missing properties.\n" + property_failure_index);
+}
+if(type_failure_index.length !== 0){
+  alert("Pokemons at the following indexes are not added as its not an object.\n" + type_failure_index);
+}
+// for each element in the pokemonlist invoke displayPokemonNames function to display its details
+//console.log(pokemonRepository.getAll());
 (pokemonRepository.getAll()).forEach(displayPokemonNames);
