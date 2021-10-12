37e1e88ec86b7b76bcf21aa30b4e05eede61af69
Unna27 <unnaweb27@gmail.com>
2021-10-12 09:54:25 +0200

    Modified the code to use forEach loop instead of normal for loop
    implemented forEach loop with callback function.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// Declare handle to access table body
+// Declare handle to access unordered list
 let datalist_row = document.getElementsByClassName('datatlist');
-// An array to store the pokemon details. Objects represent each pokemon properties
+// An array to store the pokemon objects. Each object has certain set of pokemon properties
 let pokemonList = [];
 let pokemon1 = {name: "Squirtle", height: 0.5, weight: 9, types: ['water']};
 let pokemon2 = {name: "Beedrill", height: 1, weight: 29.5, types: ['bug','poison']};
@@ -10,15 +10,17 @@ let pokemon5 = {name: "Meganium", height: 1.8, weight: 100.5, types: ['grass']};
 pokemonList = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5];
 
 console.log(pokemonList);
-// to display all the Pokemon names
-//let datalist_ul = document.createElement('ul');
-for(let i=0; i<pokemonList.length; i++){
+// function to display all the Pokemon names that accepts two args (pokemon obj and its index in the array)
+function displayPokemonNames(pokemon, i) {
   let li=document.createElement('li');
-  if(pokemonList[i].height > 1.5){
-    li.innerHTML = pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!';
+  // if height is greater than 1.5, set it as special pokemon
+  if(pokemon.height > 1.5){
+    li.innerHTML = (i + 1) + '. '+ pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!';
     li.setAttribute("class","spl_pokemon");
   } else {
-    li.innerHTML = pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')';
+    li.innerHTML = (i +1 ) + '. '+ pokemon.name + ' (height: ' + pokemon.height + ')';
   }
   datalist_row[0].appendChild(li);
 }
+// for each element in the pokemonlist invoke displayPokemonNames function to display its details
+pokemonList.forEach(displayPokemonNames);
